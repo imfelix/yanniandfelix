170c67b5dae4d52cf04b00a89378fd293857eba1
Felix Lee <felix@netengine.com.au>
2017-04-03 21:57:01 +1000

    Updated meta and adding twitter summary image

--- config.rb
+++ config.rb
@@ -22,3 +22,13 @@ configure :development do
   activate  :livereload
   activate  :syntax
 end
+
+#################################################
+## Build Configuration
+#################################################
+configure :build do
+  activate  :minify_css
+  activate  :minify_javascript
+  activate  :relative_assets
+  set       :relative_links, true
+end
